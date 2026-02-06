a217f1953fbd10c529943fc035265e34fe758006
Saransh10032006 <choudharysaransh1234@gmail.com>
2026-02-07 00:13:16 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+const App = () => (
+    <>
+      <Header />
+      <Footer />
+    </>
+   
+  )
+
+export default App;
